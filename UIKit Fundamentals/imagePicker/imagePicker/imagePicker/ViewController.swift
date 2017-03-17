2e9265fdbab511385398dc5b402fa4329a4ab33c
FaFa <farshad.faradji@gmail.com>
2017-03-17 11:56:53 +0100

    func pickImageFromAlbum added

--- UIKit Fundamentals/imagePicker/imagePicker/imagePicker/ViewController.swift
+++ UIKit Fundamentals/imagePicker/imagePicker/imagePicker/ViewController.swift
@@ -8,20 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UINavigationControllerDelegate ,UIImagePickerControllerDelegate {
 
+    
     @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        } else {
+            // Error Message
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
   
     @IBAction func pickImageFromAlbum(_ sender: UIBarButtonItem) {
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+        
     }
 
     @IBAction func pickImageFromCamera(_ sender: UIBarButtonItem) {
