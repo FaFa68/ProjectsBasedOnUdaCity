217844e58b6976561e1e99b58145359136787979
FaFa <farshad.faradji@gmail.com>
2017-04-03 13:21:50 +0200

    Fire auth added, log in and sig up features added

--- UIKit Fundamentals/FireBaseApp/FireBaseApp/FireBaseApp/ViewController.swift
+++ UIKit Fundamentals/FireBaseApp/FireBaseApp/FireBaseApp/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
-
+import  FirebaseAuth
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setButtonTittle(segmentControl)
     }
 
+    @IBAction func action(_ sender: UIButton) {
+        
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            if segmentControl.selectedSegmentIndex == 0 { // Sign in Segment
+                FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {(user , error) in
+                    if user != nil {
+                        // Sign in successful
+                        print("Succesfully Signed in")
+                        
+                    } else {
+                        if let err = error?.localizedDescription {
+                            
+                            print(err)
+                        }
+                        else {
+                            print("Error")
+                        }
+                    }
+                })
+                
+            } else {        // Sign up Segment
+                
+                FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {(user , error) in
+                    if user != nil {
+                        // Sign in successful
+                        print("Successfully Signed up")
+                    } else {
+                        if let err = error?.localizedDescription {
+                            
+                            print(err)
+                        }
+                        else {
+                            print("Error")
+                        }
+                    }
+                })
+            }
+        }
+    }
+    @IBAction func setButtonTittle(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            actionButton.setTitle("Login", for: .normal)
+        } else {
+            actionButton.setTitle("Sign Up", for: .normal)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
