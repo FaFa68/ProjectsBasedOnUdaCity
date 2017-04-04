2e4f9af7b0e2ac3b496cc7fb21d48d9f64fcf83c
FaFa <farshad.faradji@gmail.com>
2017-04-04 19:00:43 +0200

    a navigation bar and log out button added to the LoggedInViewController

---
+++ UIKit Fundamentals/FireBaseApp/FireBaseApp/FireBaseApp/PlanTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlanTableViewCell.swift
+//  FireBaseApp
+//
+//  Created by Farshad Faradji on 03/04/17.
+//  Copyright © 2017 FaFa. All rights reserved.
+//
+
+import UIKit
+
+class PlanTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
